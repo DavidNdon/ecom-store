9e23d1b0d778531b0443b1fe18b2ec74a8cab271
David N. <davidnjorogemesi@gmail.com>
2026-03-19 14:30:57 +0300

    Update connectDB.js

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -7,5 +7,5 @@ const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 export const sql = neon(
   `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require&channel_binding=require`
   
-  
+);
 
